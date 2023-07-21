2de150a01472aab302789af17baa95f37a717a5a
unlashiv <128310939+unlashiv@users.noreply.github.com>
2023-07-21 15:40:46 +0200

    post-install script is modified to import branched stuff

--- post-installation-script.sh
+++ post-installation-script.sh
@@ -4,6 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install git
 cd ~
-git clone https://github.com/unlashiv/post-installation-script.git
+git clone --branch exovision --single-branch https://github.com/unlashiv/post-installation-script/tree/exovision.git
 cd post-installation-script
 bash setup.sh
